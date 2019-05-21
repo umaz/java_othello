3774cd7594c2f0a8894e3fafddb7ff504aae6b89
yuhei matsuda <yuhei.matsuda.2017b@mlab.info>
2019-05-21 21:03:10 +0900

    COM lv4

--- Player.java
+++ Player.java
@@ -33,6 +33,9 @@ class Player {
           cell = lv3(board);
           break;
 
+        case 4:
+          cell = lv4(board);
+
         default:
           cell = lv1(board);
           break;
@@ -100,23 +103,89 @@ class Player {
         undo[i] = board.get_board()[i].clone();
       }
       board.reverse(cell[0], cell[1], color);
+      int score = board_score(board);
+      board.undo(undo);
+      evaluate(cell, score, d);
+    }
+    int[] put_cell = select_com_move(d.candicate_cells);
+    return put_cell;
+  }
+
+  private int[] lv4(Board board) {
+    ArrayList<int[]> putable_cells = get_putable_cells(board);
+    Default d = new Default();
+    int depth = 5;
+    for (int[] cell : putable_cells) {
+      int[][] undo = new int[10][];
+      for (int i = 0; i < 10; i++) {
+        undo[i] = board.get_board()[i].clone();
+      }
+      board.reverse(cell[0], cell[1], color);
       int score = 0;
-      int i = 0;
-      for(int[] row : board.get_board()) {
-        int j = 0;
-        for(int col : row) {
-          if(col == color) {
-            score += Constant.BOARD_SCORE[i][j];
-          }
-          j++;
-        }
-        i++;
+      switch (status(board, -color, depth)) {
+        case Constant.FINISH:
+          score =  board_score(board);         
+          break;
+      
+        case Constant.PASS:
+          score = minmax(board, depth-1, color);
+          break;
+
+        case Constant.MOVE:
+          score = minmax(board, depth-1, -color);
+          break;
+
+        default:
+          score = 0;
+          break;
       }
       board.undo(undo);
       evaluate(cell, score, d);
     }
     int[] put_cell = select_com_move(d.candicate_cells);
     return put_cell;
+  }
+
+  private int minmax(Board board, int depth, int color) {
+    int best_score = 999999999;
+    ArrayList<int[]> putable_cells = board.get_putable_cells(color);
+    for (int[] cell : putable_cells) {
+      int[][] undo = new int[10][];
+      for (int i = 0; i < 10; i++) {
+        undo[i] = board.get_board()[i].clone();
+      }
+      board.reverse(cell[0], cell[1], color);
+      int score = 0;
+      switch(status(board, -color, depth)) {
+        case Constant.FINISH:
+          score =  board_score(board);         
+          break;
+      
+        case Constant.PASS:
+          score = minmax(board, depth-1, color);
+          break;
+
+        case Constant.MOVE:
+          score = minmax(board, depth-1, -color);
+          break;
+
+        default:
+          score = 0;
+          break;
+      }
+      board.undo(undo);
+      //スコアの選択(αβ法)
+      if (best_score == 999999999) {
+        best_score = score;
+      }
+      if (color == this.color && score > best_score) {
+        best_score = score;
+      }
+      if (color == -this.color && score < best_score) {
+        best_score = score;
+      }
+    }
+    return best_score;
   }
 
   private ArrayList<int[]> get_putable_cells(Board board) {
@@ -139,6 +208,38 @@ class Player {
     int r = rnd.nextInt(candicate_cells.size());
     int[] cell = candicate_cells.get(r);
     return cell;
+  }
+
+  private int board_score(Board board) {
+    int score = 0;
+    int i = 0;
+    for (int[] row : board.get_board()) {
+      int j = 0;
+      for (int col : row) {
+        if (col == color) {
+          score += Constant.BOARD_SCORE[i][j];
+        }
+        j++;
+      }
+      i++;
+    }
+    return score;
+  }
+
+  private int status(Board board, int color, int depth) {
+    if (depth == 0) {
+      return Constant.FINISH;
+    } else {
+      if (board.get_putable_cells(color).size() == 0) {
+        if (board.get_putable_cells(-color).size() == 0) {
+          return Constant.FINISH;
+        } else {
+          return Constant.PASS;
+        }
+      } else {
+        return Constant.MOVE;
+      }
+    }
   }
 }
 
